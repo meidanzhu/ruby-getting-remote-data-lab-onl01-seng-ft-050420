8dcd71e58087c554583a0cacdcedca5700e846f1
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-21 21:11:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
-# Write your code here
+require "net/http"
+require "open-uri"
+require "json"
+  
+end
+
+
+class GetRequester
+    attr_accessor :name, :occupation
+
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def initialization(url)
+    @url = url
+  end  
+    
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end 
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
+
